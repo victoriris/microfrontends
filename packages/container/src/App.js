9c08ab7fa5720d624401bf4e92babc568bf39500
Victor Iris <victormidp@gmail.com>
2020-12-27 15:31:45 -0600

    Add build invalidation step

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return (
         <div>
-            <h1>hello</h1>
+            <h1>Container App</h1>
             <hr />
             <MarketingApp />
         </div>
