73e6117cd5ba0ea100aca7b532297c7013bf3b31
Victor Iris <victormidp@gmail.com>
2020-12-27 16:33:46 -0600

    Set custom prod classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
+import {
+	StylesProvider,
+	createGenerateClassName,
+} from "@material-ui/core/styles";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: "co",
+});
+
 export default () => {
 	return (
-		<BrowserRouter>
-			<div>
-				<Header />
-				<MarketingApp />
-			</div>
-		</BrowserRouter>
+		<StylesProvider generateClassName={generateClassName}>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<MarketingApp />
+				</div>
+			</BrowserRouter>
+		</StylesProvider>
 	);
 };
