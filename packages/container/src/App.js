b998dcb0daf860312af01a05b3ce6c918384a33b
Victor Iris <victormidp@gmail.com>
2020-12-30 15:58:34 -0600

    Create dashboard app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,37 +1,54 @@
 import {
-	StylesProvider,
 	createGenerateClassName,
+	StylesProvider,
 } from "@material-ui/core/styles";
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { Redirect, Route, Router, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
 
 const AuthLazy = lazy(() => import("./components/AuthApp"));
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
 	productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
 	const [isSignedIn, setIsSignedIn] = useState(false);
 
+	useEffect(() => {
+		if (isSignedIn) {
+			history.push("/dashboard");
+		}
+	}, [isSignedIn]);
+
 	return (
-		<BrowserRouter>
-		<StylesProvider generateClassName={generateClassName}>
+		<Router history={history}>
+			<StylesProvider generateClassName={generateClassName}>
 				<div>
-					<Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
+					<Header
+						onSignOut={() => setIsSignedIn(false)}
+						isSignedIn={isSignedIn}
+					/>
 					<Suspense fallback={<Progress />}>
 						<Switch>
 							<Route path="/auth">
 								<AuthLazy onSignIn={() => setIsSignedIn(true)} />
 							</Route>
+							<Route path="/dashboard">
+								{!isSignedIn && <Redirect to="/" />}
+								<DashboardLazy />
+							</Route>
 							<Route path="/" component={MarketingLazy} />
 						</Switch>
 					</Suspense>
 				</div>
-		</StylesProvider>
-			</BrowserRouter>
+			</StylesProvider>
+		</Router>
 	);
 };
